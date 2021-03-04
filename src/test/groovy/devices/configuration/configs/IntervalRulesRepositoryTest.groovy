5c747be3918cc693807d8d57d762c1d7a1ce48ad
Michal MIchaluk <michal.michaluk@bottega.com.pl>
2021-03-04 14:25:48 +0100

    Zadanie 2.

---
+++ src/test/groovy/devices/configuration/configs/IntervalRulesRepositoryTest.groovy
@@ -0,0 +1,29 @@
+package devices.configuration.configs
+
+import devices.configuration.IntegrationTest
+import spock.lang.Specification
+
+import javax.transaction.Transactional
+
+@IntegrationTest
+@Transactional
+class IntervalRulesRepositoryTest extends Specification {
+
+//    @Autowired
+//    private IntervalRulesRepository repository
+//    @Autowired
+//    private FeaturesConfigurationRepository db
+
+    def "Should fetch"() {
+//        given:
+//        transactional { db.save(currentRules()) }
+//
+//        when:
+//        IntervalRules actual = transactional({ repository.get() } as Supplier)
+//
+//        then:
+//        JsonAssert.assertThat(actual)
+//                .hasFieldsLike(...)
+    }
+
+}
